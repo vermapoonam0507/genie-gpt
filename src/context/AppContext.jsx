bf5741dcfed71c14a92bd422cc6a5d785bd9309f
Poonam Verma <vermapoonam0507@gmail.com>
2025-08-30 18:38:10 +0530

    Solve the bug from chat section

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,17 +1,59 @@
-import { createContext, useContext } from 'react';
+import { createContext, useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyUserData } from "../assets/assets";
 
 const AppContext = createContext();
 
+export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [chats, setChats] = useState([]);
+  const [selectedChat, setSelectedChat] = useState(null);
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
-export const AppContextProvider =({children}) =>{
+  const fetchUser = async () => {
+    setUser(dummyUserData);
+  };
+  const fetchUsersChats = async () => {
+    setChats(dummyChats);
+    setSelectedChat(dummyChats[0]);
+  };
 
-    const value = {}
-     return (
-        <AppContext.Provider value={value}>
-            {children}
-        </AppContext.Provider>
-     )
-}
+  useEffect(() =>{
+    if(theme === "dark"){
+        document.documentElement.classList.add("dark");
+    }else{
+        document.documentElement.classList.remove("dark");
+    }
+  }, [theme])
 
 
-export const useAppContext =() =>useContext(AppContext)
+  useEffect(() => {
+    if(user){
+        fetchUsersChats();
+    }
+    else{
+        setChats([])
+        setSelectedChat(null);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const value = {
+    navigate,
+    user,
+    setUser,
+    fetchUser,
+    chats,
+    setChats,
+    selectedChat,
+    setSelectedChat,
+    theme,
+  };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => useContext(AppContext);
